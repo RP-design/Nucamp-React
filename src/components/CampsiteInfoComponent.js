a8df6d78ba358e740e5e7699412f3b87958a55c3
Rupali Pawar <rupaliapawar06@gmail.com>
2020-09-28 14:29:42 -0500

    redux thunk and logger

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody,  Breadcrumb, BreadcrumbItem, Button,Modal, ModalHeader, ModalBody, FormGroup, Label} from 'reactstrap';
 import { Control, LocalForm, Errors} from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 
     const required = val => val && val.length;
@@ -150,6 +151,27 @@ import { Link } from 'react-router-dom';
     }
 
     function CampsiteInfo(props) {
+
+        if (props.isLoading) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        if (props.errMess) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <h4>{props.errMess}</h4>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
         
         if (props.campsite) {
             return (
